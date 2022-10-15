c9f0a7b14222fa15073366f8ca394c4740636b58
artilrobotics <artilrobotics@gmail.com>
2022-10-14 21:59:42 -0500

    Agregar imagenes Hector 9000

--- Hector9000/conf/drinks.py
+++ Hector9000/conf/drinks.py
@@ -4,7 +4,7 @@ drink_list = [
     {
         "name": "Piña colada",
         "recipe": [("ingr", "mate", 50)],
-        "image": "https://www.elcolombiano.com/binrepository/470x604/0c19/470d565/none/11101/QXGP/bad-bunny-concierto-instagram_40231022_20220630145812.jpg"
+        "image": "../Hector9000/Images/pina-colada.png"
     },{
         "name": "Margarita frozen de fresa",
         "recipe": [
@@ -12,7 +12,7 @@ drink_list = [
             ("ingr", "lime", 10),
             ("ingr", "gibe", 180),
         ],
-        "image": "https://www.pngfind.com/pngs/m/626-6261425_margarita-de-fresa-coctel-margarita-de-fresa-hd.png"
+        "image": "../Hector9000/Images/margarita-fresa.png"
     } ,{
         "name": "Margarita frozen de limón",
         "recipe": [
@@ -20,7 +20,7 @@ drink_list = [
             ("ingr", "lime", 10),
             ("ingr", "gibe", 180),
         ],
-        "image": "https://w7.pngwing.com/pngs/421/515/png-transparent-margarita-cocktail-slush-fizzy-drinks-liqueur-tangy-food-recipe-non-alcoholic-beverage-thumbnail.png"
+        "image": "../Hector9000/Images/margarita-frozen.png"
     }, {
         "name": "Pisco Sunrise",
         "color": "white",
@@ -28,7 +28,7 @@ drink_list = [
             ("ingr", "gin", 40),
             ("ingr", "tonic", 120),
         ],
-        "image": "https://e7.pngegg.com/pngimages/885/148/png-clipart-tequila-sunrise-cocktail-margarita-sangria-cocktail-food-recipe-thumbnail.png"
+        "image": "../Hector9000/Images/pisco-sunrise.png"
     }, {
         "name": "Cosmopolitan",
         "color": "orange",
@@ -37,7 +37,7 @@ drink_list = [
             ("ingr", "oj", 120),
             ("stir", True),
         ],
-        "image": "https://img2.freepng.es/20180622/xqq/kisspng-cosmopolitan-cocktail-martini-soju-vodka-5b2d1a8ba8fcd5.3191597715296825716922.jpg"
+        "image": "../Hector9000/Images/cosmopolitan.png"
     }, {
         "name": "Mojito",
         "color": "red",
@@ -46,7 +46,7 @@ drink_list = [
             ("ingr", "gren", 15),
             ("umb", True),
         ],
-        "image": "https://img2.freepng.es/20180403/cfe/kisspng-mojito-cocktail-juice-fizzy-drinks-beer-mojito-5ac3d281e8f5d1.7511593015227828499542.jpg"
+        "image": "../Hector9000/Images/mojito.png"
     }
 ]
 
